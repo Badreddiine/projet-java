fc4d00d8f8b5cb0c47323819fbf88ddb120834cd
khalilbaidouri <khalilbaidouri97@gmail.com>
2025-05-07 15:32:44 +0000

    Add files via upload

---
+++ src/main/java/com/example/javaprojet/entity/ProjetRole.java
@@ -0,0 +1,59 @@
+package com.example.javaprojet.entity;
+
+import com.example.javaprojet.model.RoleType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProjetRole {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Utilisateur utilisateur;
+
+    @ManyToOne
+    private Projet projet;
+
+    @Enumerated(EnumType.STRING)
+    private RoleType role;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Utilisateur getUtilisateur() {
+        return utilisateur;
+    }
+
+    public void setUtilisateur(Utilisateur utilisateur) {
+        this.utilisateur = utilisateur;
+    }
+
+    public Projet getProjet() {
+        return projet;
+    }
+
+    public void setProjet(Projet projet) {
+        this.projet = projet;
+    }
+
+    public RoleType getRole() {
+        return role;
+    }
+
+    public void setRole(RoleType role) {
+        this.role = role;
+    }
+}
